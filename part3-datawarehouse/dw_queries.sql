0c5619a5d249bfbbd99ef1dfbec5f63879f1ccec
nikitapatwari2208 <44616441+nikitapatwari2208@users.noreply.github.com>
2026-03-23 18:53:02 +0530

    Delete part3-datawarehouse directory

--- part3-datawarehouse/dw_queries.sql
+++
@@ -1,9 +0,0 @@
--- Q1 Sales revenue by category monthly
-
-SELECT d.month,
-p.category,
-SUM(f.revenue)
-FROM fact_sales f
-JOIN dim_date d ON f.date_id = d.date_id
-JOIN dim_product p ON f.product_id = p.product_id
-GROUP BY d.month, p.category;
